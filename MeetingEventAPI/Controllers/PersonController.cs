b3d897ee5cdc5a742f27021192dc2e11f8f50e5f
katfrankowska <66004567+katfrankowska@users.noreply.github.com>
2021-04-29 20:11:58 +0200

    person complete

--- MeetingEventAPI/Controllers/PersonController.cs
+++ MeetingEventAPI/Controllers/PersonController.cs
@@ -4,12 +4,74 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MeetingEventAPI.Services;
+using AutoMapper;
+using MeetingEventAPI.Entities;
+using MeetingEventAPI.Models;
 
 namespace MeetingEventAPI.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/person")]
     [ApiController]
     public class PersonController : ControllerBase
     {
+        private readonly IPersonService _personService;
+
+
+        public PersonController(IPersonService personService)
+        {
+            _personService = personService;
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult Update([FromBody] UpdatePersonDto dto, [FromRoute] int id)
+        {
+            var isUpdated = _personService.Update(id, dto);
+            if (isUpdated)
+            {
+                return NotFound();
+            }
+
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete([FromRoute] int id)
+        {
+            var isDeleted = _personService.Delete(id);
+            if (isDeleted)
+            {
+                return NoContent();
+            }
+
+            return NotFound();
+        }
+
+        [HttpPost]
+        public ActionResult CreatePerson([FromBody] CreatePersonDto dto)
+        {
+            var id = _personService.Create(dto);
+            return Created($"/api/event/{id}", null);
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<PersonDto>> GetAll()
+        {
+
+            var people = _personService.GetAll();
+            return Ok();
+        }
+        [HttpGet("{id}")]
+        public ActionResult<PersonDto> Get([FromRoute] int id)
+        {
+            var people = _personService.GetById(id);
+            if (people is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(people);
+        }
+
     }
 }
--- MeetingEventAPI/Services/PersonService.cs
+++ MeetingEventAPI/Services/PersonService.cs
@@ -2,10 +2,91 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MeetingEventAPI.Models;
+using MeetingEventAPI.Entities;
+using AutoMapper;
+using MeetingEventAPI.Controllers;
+using Microsoft.EntityFrameworkCore;
 
 namespace MeetingEventAPI.Services
 {
-    public class PersonService
+    public interface IPersonService
     {
+        PersonDto GetById(int id);
+        IEnumerable<PersonDto> GetAll();
+        int Create(CreatePersonDto dto);
+        bool Delete(int id);
+        bool Update(int id, UpdatePersonDto dto);
+    }
+
+    public class PersonService : IPersonService
+    {
+        private readonly EventDbContext _dbContext;
+        private readonly IMapper _mapper;
+
+        public PersonService(EventDbContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+        public bool Update(int id, UpdatePersonDto dto)
+        {
+            var people = _dbContext
+                .People
+                .FirstOrDefault(e => e.Id == id);
+            if (people is null)
+                return false;
+            people.Name = dto.Name;
+            people.Surname = dto.Surname;
+            people.Email = dto.Email;
+            people.BirthDate = dto.BirthDate;
+            _dbContext.SaveChanges();
+            return true;
+
+        }
+
+        public bool Delete(int id)
+        {
+            var people = _dbContext
+                .People
+                .FirstOrDefault(e => e.Id == id);
+            if (people is null) return false;
+            _dbContext.People.Remove(people);
+            _dbContext.SaveChanges();
+            return true;
+        }
+
+        public PersonDto GetById(int id)
+        {
+            var people = _dbContext
+                .People
+                .FirstOrDefault(e => e.Id == id);
+            if (people is null) return null;
+            var result = _mapper.Map<PersonDto>(people);
+            return result;
+        }
+
+        public IEnumerable<PersonDto> GetAll()
+        {
+            var people = _dbContext
+                .People
+                .ToList();
+            var peopleDtos = _mapper.Map<List<PersonDto>>(people);
+            return peopleDtos;
+
+        }
+
+        public int Create(CreatePersonDto dto)
+        {
+            var people = _mapper.Map<Person>(dto);
+            _dbContext.People.Add(people);
+            _dbContext.SaveChanges();
+            return people.Id;
+        }
+
+
+
     }
 }
+
